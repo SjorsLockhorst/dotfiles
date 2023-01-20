a79a396a1ea89d93ebc4b3c6cbacfd695234c7ee
Sjors Lockhorst <lockhorstsjors@gmail.com>
2023-01-20 21:17:48 +0100

    Updated telescope to work

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -6,9 +6,10 @@ local builtin = require('telescope.builtin')
 
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<C-f>', builtin.git_files, {})
+-- vim.keymap.set('n', '<leader>ps', function()
+	-- builtin.grep_string({ search = vim.fn.input("Grep > ") });
+-- end)
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-vim.keymap.set("n", "<leader>w", "<cmd>Telescope workspaces<cr>")
+vim.keymap.set('n', '<leader>w', '<cmd>Telescope workspaces<cr>')
