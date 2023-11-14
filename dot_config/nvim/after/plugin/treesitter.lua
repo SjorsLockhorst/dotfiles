32040f5721e80f713ce49c16a5afa741890bf6ad
Sjors Lockhorst <sjors.lockhorst@memo2.nl>
2023-11-14 13:54:51 +0100

    Added stuff for MeMo config

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -3,7 +3,7 @@ require 'nvim-treesitter.configs'.setup {
         enable = true,
     },
     -- A list of parser names, or "all" (the four listed parsers should always be installed)
-    ensure_installed = { "c", "python", "vue", "typescript", "lua", "vim" },
+    ensure_installed = { "c", "python", "vue", "typescript", "lua", "vim", "rst"},
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     highlight = {
--- dot_config/nvim/lua/base/sets.lua
+++ dot_config/nvim/lua/base/sets.lua
@@ -49,3 +49,20 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+-- Specific clipboard for wsl
+if vim.fn.has("wsl") == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
+
