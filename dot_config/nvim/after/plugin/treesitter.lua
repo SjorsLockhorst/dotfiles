7d8421933c9c74b89bc37267ecdb7aae77011c8a
Sjors Lockhorst <sjors@lockhorst.dev>
2023-12-21 14:21:41 +0200

    Added javascript and css support

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -3,7 +3,7 @@ require 'nvim-treesitter.configs'.setup {
         enable = true,
     },
     -- A list of parser names, or "all" (the four listed parsers should always be installed)
-    ensure_installed = { "c", "python", "vue", "typescript", "lua", "vim", "rst", "markdown"},
+    ensure_installed = { "c", "python", "vue", "typescript", "lua", "vim", "html"," javascript", "rst", "markdown"},
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     highlight = {
