c2ad813a534bc7b7f033d7078469b87d90c95296
Sjors Lockhorst <sjors@lockhorst.dev>
2024-02-18 19:45:22 +0100

    Made obsidian nvim be able to open links

--- dot_config/nvim/after/plugin/obsidian.lua
+++ dot_config/nvim/after/plugin/obsidian.lua
@@ -18,4 +18,8 @@ require("obsidian").setup({
     -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
     template = "Daily.md"
   },
+  follow_url_func = function(url)
+    -- Open the URL in the default web browser.
+    vim.fn.jobstart({"xdg-open", url})  -- linux
+  end,
 })
