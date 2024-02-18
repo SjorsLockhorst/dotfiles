96e87e6247436fa8dc9dd5964d4fb2a4e2c0c0b4
Sjors Lockhorst <sjors@lockhorst.dev>
2024-02-18 16:36:03 +0100

    Added correct daily note config for obsidian

--- dot_config/nvim/after/plugin/obsidian.lua
+++ dot_config/nvim/after/plugin/obsidian.lua
@@ -1,8 +1,21 @@
- require("obsidian").setup({
-      workspaces = {
-        {
-          name = "personal",
-          path = "~/vaults/personal",
-        },
-      },
- })
+require("obsidian").setup({
+  workspaces = {
+    {
+      name = "lifeOS",
+      path = "~/vaults/lifeOS/",
+    },
+  },
+  templates = {
+    subdir = "0. PeriodicNotes/Templates",
+    date_format = "%Y-%m-%d",
+    time_format = "%Hh:%M",
+  },
+  daily_notes = {
+    -- Optional, if you keep daily notes in a separate directory.
+    folder = os.date("0. PeriodicNotes/%Y/Daily/%m"),
+    -- Optional, if you want to change the date format for the ID of daily notes.
+    date_format = "%Y-%m-%d",
+    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+    template = "Daily.md"
+  },
+})
