82355134521f873a2530c0a25fe93d4ef277c21a
Sjors Lockhorst <sjors@lockhorst.dev>
2024-02-19 22:05:49 +0100

    Removed gitsigns ugly setup

--- dot_config/nvim/after/plugin/gitsigns.lua
+++ dot_config/nvim/after/plugin/gitsigns.lua
@@ -1,11 +1,4 @@
 -- Gitsigns
 -- See `:help gitsigns.txt`
 require('gitsigns').setup {
-  signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-  },
 }
