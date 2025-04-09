05e373795c7250419c994eafdcf2db33bed0f8fe
Sjors Lockhorst <sjors@lockhorst.dev>
2025-04-09 15:26:18 +0200

    Made avante use 3.5 sonnet, no tools

--- dot_config/nvim/lua/base/lazy.lua
+++ dot_config/nvim/lua/base/lazy.lua
@@ -160,15 +160,31 @@ require("lazy").setup({
     {
         "yetone/avante.nvim",
         event = "VeryLazy",
-        lazy = false,
-        version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+        version = false, -- Never set this value to "*"! Never!
         opts = {
+            provider = "claude", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+            -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
+            -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
+            -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
+            auto_suggestions_provider = "claude",
+            cursor_applying_provider = nil, -- The provider used in the applying phase of Cursor Planning Mode, defaults to nil, when nil uses Config.provider as the provider for the applying phase
+            claude = {
+                endpoint = "https://api.anthropic.com",
+                model = "claude-3-5-sonnet-20241022",
+                temperature = 0,
+                max_tokens = 4096,
+                disable_tools = true
+            },
             -- add any opts here
+            -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
+            -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
+            -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
         },
         -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
         build = "make",
         -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
         dependencies = {
+            "nvim-treesitter/nvim-treesitter",
             "stevearc/dressing.nvim",
             "nvim-lua/plenary.nvim",
             "MunifTanjim/nui.nvim",
