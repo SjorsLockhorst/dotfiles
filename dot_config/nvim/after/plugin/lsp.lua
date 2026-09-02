c48d526d32f4e34d749dfa452117d95bb863099d
Sjors Lockhorst <sjors@lockhorst.dev>
2026-09-02 12:18:15 +0200

    Small config additions

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -40,12 +40,6 @@ vim.lsp.config('pyright', {
   settings = {
     -- Using Ruff's import organizer
     disableOrganizeImports = true,
-    python = {
-      analysis = {
-        -- Ignore all files for analysis to exclusively use Ruff for linting
-        ignore = { '*' },
-      },
-    },
   },
 })
 
